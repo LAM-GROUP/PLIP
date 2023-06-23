af449fb845930a0e3f801e66288b1b8bca9ed406
aksam432 <akshaykrishnaak9@gmail.com>
2023-06-23 10:39:53 +0200

    refactor: Broken down existing functions to ease testing

--- src/plip/Lasso.py
+++ src/plip/Lasso.py
@@ -34,11 +34,77 @@ def readForces(input_file):
         np.array: forces per atom
     """
     tmp = np.genfromtxt(input_file, invalid_raise=False, usecols=[0, 1, 2], dtype=float)
+    if np.isnan(tmp).any():
+        raise ValueError(f"NaN value encountered in {input_file} file")
     return tmp
 
 
+def getBinaryFiles(input_file, list_itype):
+    """Get the list of binary files based on the input file and descriptor types
+
+    Args:
+        input_file (str): Input structure file
+        list_itype (list): List of descriptor types
+
+    Returns:
+        list: List of binary files
+    """
+    list_bin = []
+    for i_type in list_itype:
+        search = input_file[:-6] + "poscar_f" + str(i_type) + "_*bin"
+        search = search.replace("Refs", "Input")
+        print(search)
+        list_bin.extend(
+            glob.glob(search)
+        )  # list_bin = np.append(list_bin, sorted(glob.glob(search)))
+    return sorted(list_bin)
+
+
+def readBinaryFiles(list_bin):
+    """Read and concatenate data from binary files
+
+    Args:
+        list_bin (list): List of binary files
+
+    Returns:
+        np.array: Concatenated data from binary files
+    """
+    print(list_bin)
+    XMAT = np.genfromtxt(list_bin[0])
+    XMAT = np.transpose(XMAT)
+
+    for input_bin in list_bin[1:]:
+        XMAT_tmp = np.genfromtxt(input_bin)
+        XMAT_tmp = np.transpose(XMAT_tmp)
+        XMAT = np.concatenate((XMAT, XMAT_tmp), axis=1)
+
+    return XMAT
+
+
+def read_File(input_file, list_itype):
+    """Read the input structure file and binary files
+
+    Args:
+        input_file (str): Input structure file
+        list_itype (list): List of descriptor types
+
+    Returns:
+        np.array: Fingerprint matrix
+        np.array: Forces per atom
+    """
+    Y = readForces(input_file)
+    Y = Y.reshape(1, 3 * np.shape(Y)[0])
+    Y = Y[0]
+
+    list_bin = getBinaryFiles(input_file, list_itype)
+
+    XMAT = readBinaryFiles(list_bin)
+
+    return XMAT, Y
+
+
 # Read Input files
-def readFile(input_file, list_itype):
+def oldread_File(input_file, list_itype):
     """
 
     Args:
@@ -78,18 +144,15 @@ def readFile(input_file, list_itype):
     return (XMAT, Y)
 
 
-# LASSO PROCESS
-def runLasso(inputArgs, ref_dir="Refs"):
-    """
-
+def setup_data(inputArgs, ref_dir):
+    """Build fingerprint matrix and references
     Args:
-        inputArgs (_type_): Input file name
-        ref_dir (str, optional): reference folder name. Defaults to "Refs".
+        inputArgs (string): Descriptor type
+        ref_dir (str, optional): Folder with reference forces. Defaults to "Refs".
 
     Raises:
         ValueError: When references are empty
     """
-    print("Running LassoLars")
     list_itype = []
     str_type = ""
     for i in np.arange(len(inputArgs)):
@@ -97,8 +160,10 @@ def runLasso(inputArgs, ref_dir="Refs"):
         str_type = str_type + str(inputArgs[i])
     print(list_itype)
     print(str_type)
-    N_sample = 100
+
+    #    N_sample = 100
     #    i_select = 1
+
     # Data reading
     search = f"{ref_dir}/*.forces"
     input_file = sorted(glob.glob(search))[0]
@@ -107,11 +172,12 @@ def runLasso(inputArgs, ref_dir="Refs"):
         raise ValueError(
             "The reference directory is empty or  references not in correct format"
         )
-    XMAT, Yin = readFile(input_file, list_itype)
+    XMAT, Yin = read_File(input_file, list_itype)
+
     print(XMAT.shape, Yin.shape)
 
     for input_file in sorted(glob.glob(search))[1:]:
-        XMAT_tmp, Yin_tmp = readFile(input_file, list_itype)
+        XMAT_tmp, Yin_tmp = read_File(input_file, list_itype)
         XMAT = np.concatenate((XMAT, XMAT_tmp), axis=0)
         Yin = np.append(Yin, Yin_tmp)
         if np.shape(XMAT)[0] != np.shape(Yin)[0]:
@@ -120,6 +186,20 @@ def runLasso(inputArgs, ref_dir="Refs"):
     filter_ = abs(Yin) < 15
     XMAT = XMAT[filter_]
     Yin = Yin[filter_]
+
+    return XMAT, Yin, str_type
+
+
+# LASSO PROCESS
+def runLasso(inputArgs, ref_dir="Refs"):
+    """Run lasso Lars
+
+    Args:
+        inputArgs (string): Descriptor type
+        ref_dir (str, optional): reference folder name. Defaults to "Refs".
+    """
+    print("Running LassoLars")
+    XMAT, Yin, str_type = setup_data(inputArgs, ref_dir)
 
     # Split data in Train/Test
     N_sample = np.shape(Yin)[0]
